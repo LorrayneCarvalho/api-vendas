9f40ef01c0b04fd225b8ed533b457a72fa8dcaac
Lorrayne Costa de Carvalho <lorrayne.carvalho@ibm.com>
2023-05-24 18:33:48 -0300

    validação dos dados de requisição

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response, response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -11,20 +13,25 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
       status: 'error',
-      message: error.message,
+      message: 'Internal server error',
     });
-  }
-
-  return response.status(500).json({
-    status: error,
-    message: 'Internal Server Error',
-  });
-});
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333! :)');
