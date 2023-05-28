bd0d36d1b6b14f60ec4db38e25ba06881de11bc5
Lorrayne Costa de Carvalho <lorrayne.carvalho@ibm.com>
2023-05-28 19:51:16 -0300

    Rota de Upload de Avatar de Usuário

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -2,11 +2,18 @@ import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import UsersController from '../controllers/UsersController';
 import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', isAuthenticated, usersController.index);
+
 usersRouter.post(
   '/',
   celebrate({
@@ -17,6 +24,14 @@ usersRouter.post(
     },
   }),
   usersController.create,
+);
+
+// patch: altera um único dado do user
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
 );
 
 export default usersRouter;
